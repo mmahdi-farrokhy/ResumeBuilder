432f00b7081a781265c492cd4d679e64df01803e
mmahdi-farrokhy <mmahdifarrokhy@gmail.com>
2023-11-26 21:30:18 +0330

    Project initialized

---
+++ src/main/java/com/mmf/resumeBuilder/ResumeBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.mmf.resumeBuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeBuilderApplication.class, args);
+	}
+
+}
