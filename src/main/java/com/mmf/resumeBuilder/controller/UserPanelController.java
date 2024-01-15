5b1107f4558b8f1591385889f763bf5379ff01d5
mmahdi-farrokhy <mmahdifarrokhy@gmail.com>
2024-01-15 23:10:25 +0330

    - updated diagrams Open User Panel - Sequence Diagram, Update Profile - Sequence Diagram
    - developed UserPanelControllerShould to test delete, edit, share and download resume and update user profile
    - added profile-update-success.html, user-panel.html, user-profile.html web pages

--- src/main/java/com/mmf/resumeBuilder/controller/UserPanelController.java
+++ src/main/java/com/mmf/resumeBuilder/controller/UserPanelController.java
@@ -1,20 +1,69 @@
 package com.mmf.resumeBuilder.controller;
 
+import com.mmf.resumeBuilder.enums.UserRole;
+import com.mmf.resumeBuilder.model.AppUser;
+import com.mmf.resumeBuilder.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.ServletRequestBindingException;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/panel")
 public class UserPanelController {
-    @GetMapping("")
-    public String showUserPanel(Model model){
+    UserService userService;
+
+    @Autowired
+    public UserPanelController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/{id}")
+    public String showUserPanel(@PathVariable String id, Model model) {
+        AppUser appUser = userService.fetchUser(Integer.valueOf(id));
+        model.addAttribute("user", appUser);
         return "user-panel";
     }
 
-    @GetMapping("/resumes")
-    public String getResumeList(){
-        return "resumes";
+    @GetMapping("/delete/{resumeid}")
+    public String goToDeleteResumeEndpoint(@PathVariable String resumeid) {
+        return "redirect:/resume/delete/" + resumeid;
+    }
+
+    @GetMapping("/download/{resumeid}")
+    public String goToDownloadResumeEndpoint(@PathVariable String resumeid) {
+        return "redirect:/resume/download/" + resumeid;
+    }
+
+    @GetMapping("/edit/{resumeid}")
+    public String goToEditResumeEndpoint(@PathVariable String resumeid) {
+        return "redirect:/resume/edit/" + resumeid;
+    }
+
+    @GetMapping("/share/{resumeid}")
+    public String goToShareResumeEndpoint(@PathVariable String resumeid) {
+        return "redirect:/resume/share/" + resumeid;
+    }
+
+    @GetMapping("/{userid}/profile")
+    public String showUserProfile(@PathVariable String userid, Model model) {
+        AppUser appUser = userService.fetchUser(Integer.valueOf(userid));
+        model.addAttribute("user", appUser);
+        return "user-profile";
+    }
+
+    @PostMapping("/profile/update")
+    public String updateUser(@Valid @ModelAttribute("user") AppUser user, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "user-profile";
+        } else {
+            user.setRole(UserRole.User);
+            userService.saveUser(user);
+            model.addAttribute("user", user);
+            return "profile-update-success";
+        }
     }
 }
--- src/test/java/com/mmf/resumeBuilder/controller/UserPanelControllerShould.java
+++ src/test/java/com/mmf/resumeBuilder/controller/UserPanelControllerShould.java
@@ -1,16 +1,29 @@
 package com.mmf.resumeBuilder.controller;
 
+import com.mmf.resumeBuilder.data.dao.UserDAO;
+import com.mmf.resumeBuilder.enums.UserRole;
+import com.mmf.resumeBuilder.model.AppUser;
+import com.mmf.resumeBuilder.service.UserService;
 import org.junit.jupiter.api.*;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.web.servlet.ModelAndView;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.ModelAndViewAssert.assertModelAttributeValue;
 import static org.springframework.test.web.ModelAndViewAssert.assertViewName;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
@@ -20,25 +33,149 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class UserPanelControllerShould {
     @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
     MockMvc mockMvc;
+
+    @MockBean
+    UserService userService;
+
+    @MockBean
+    UserDAO userDAO;
+
+    private AppUser expectedUser1;
+    private AppUser expectedUser2;
+
+    @BeforeEach
+    public void init() {
+        jdbcTemplate.execute("INSERT INTO app_user (email, first_name, last_name, password, role) VALUES ('mmahdifarrokhy@gmail.com', 'Mohammadmahdi', 'Farrokhy', '12345679', 'User')");
+        jdbcTemplate.execute("INSERT INTO app_user (email, first_name, last_name, password, role) VALUES ('bradpitt@gmail.com', 'Brad', 'Pitt', '12345679', 'User')");
+        jdbcTemplate.execute("INSERT INTO app_user (email, first_name, last_name, password, role) VALUES ('davidbeckham78@gmail.com', 'David', 'Beckham', '12345679', 'User')");
+        jdbcTemplate.execute("INSERT INTO app_user (email, first_name, last_name, password, role) VALUES ('arashrahmani@gmail.com', 'آرش', 'رحمانی', '12345679', 'User')");
+
+        expectedUser1 = new AppUser();
+        expectedUser1.setFirstName("Mohammadmahdi");
+        expectedUser1.setLastName("Farrokhy");
+        expectedUser1.setPassword("12345679");
+        expectedUser1.setPasswordConfirmation("12345679");
+        expectedUser1.setEmail("mmahdifarrokhy@gmail.com");
+        expectedUser1.setRole(UserRole.User);
+
+        expectedUser2 = new AppUser();
+        expectedUser2.setFirstName("Brad");
+        expectedUser2.setLastName("Pitt");
+        expectedUser2.setEmail("bradpitt@gmail.com");
+        expectedUser2.setPassword("12345679");
+        expectedUser2.setPasswordConfirmation("12345679");
+        expectedUser2.setRole(UserRole.User);
+    }
+
+    @AfterEach
+    public void reset() {
+        jdbcTemplate.execute("DELETE FROM app_user");
+    }
 
     @Test
     @Order(0)
-    void open_user_panel_page() throws Exception {
-        MvcResult mvcResult = mockMvc.perform(get("/panel"))
+    void open_user_panel_html_on_request_to_endpoint_panel_id() throws Exception {
+        when(userService.fetchUser(1)).thenReturn(expectedUser1);
+        when(userService.fetchUser(2)).thenReturn(expectedUser2);
+
+        MvcResult mvcResult = mockMvc.perform(get("/panel/1"))
                 .andExpect(status().isOk())
                 .andReturn();
         ModelAndView modelAndView = mvcResult.getModelAndView();
         assertViewName(modelAndView, "user-panel");
+        assertModelAttributeValue(modelAndView, "user", expectedUser1);
+        verify(userService, times(1)).fetchUser(1);
     }
 
     @Test
-    void show_user_resume_list() throws Exception {
-        MvcResult mvcResult = mockMvc.perform(get("/panel/resumes"))
-                .andExpect(status().isOk())
+    void redirect_to_endpoint_resume_delete_id_on_request_to_endpoint_panel_delete_resumeid() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(get("/panel/delete/3"))
+                .andExpect(status().is(302))
                 .andReturn();
 
         ModelAndView modelAndView = mvcResult.getModelAndView();
-        assertViewName(modelAndView, "resumes");
+        assertViewName(modelAndView, "redirect:/resume/delete/3");
+    }
+
+    @Test
+    void redirect_to_endpoint_resume_download_id_on_request_to_endpoint_panel_download_resumeid() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(get("/panel/download/3"))
+                .andExpect(status().is(302))
+                .andReturn();
+
+        ModelAndView modelAndView = mvcResult.getModelAndView();
+        assertViewName(modelAndView, "redirect:/resume/download/3");
+    }
+
+    @Test
+    void redirect_to_endpoint_resume_edit_id_on_request_to_endpoint_panel_edit_resumeid() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(get("/panel/edit/3"))
+                .andExpect(status().is(302))
+                .andReturn();
+
+        ModelAndView modelAndView = mvcResult.getModelAndView();
+        assertViewName(modelAndView, "redirect:/resume/edit/3");
+    }
+
+    @Test
+    void redirect_to_endpoint_resume_share_id_on_request_to_endpoint_panel_share_resumeid() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(get("/panel/share/3"))
+                .andExpect(status().is(302))
+                .andReturn();
+
+        ModelAndView modelAndView = mvcResult.getModelAndView();
+        assertViewName(modelAndView, "redirect:/resume/share/3");
+    }
+
+    @Test
+    @Order(0)
+    void open_user_profile_html_on_request_to_endpoint_panel_id_profile() throws Exception {
+        when(userService.fetchUser(1)).thenReturn(expectedUser1);
+        when(userService.fetchUser(2)).thenReturn(expectedUser2);
+
+        MvcResult mvcResult = mockMvc.perform(get("/panel/2/profile"))
+                .andExpect(status().isOk())
+                .andReturn();
+        ModelAndView modelAndView = mvcResult.getModelAndView();
+        assertViewName(modelAndView, "user-profile");
+        assertModelAttributeValue(modelAndView, "user", expectedUser2);
+        verify(userService, times(1)).fetchUser(2);
+    }
+
+    @Test
+    @Order(1)
+    void redirect_to_endpoint_panel_profile_update_success_on_request_to_endpoint_panel_profile_update() throws Exception {
+        AppUser updatedUser = new AppUser();
+        updatedUser.setFirstName("Mohammad Mahdi");
+        updatedUser.setLastName(expectedUser1.getLastName());
+        updatedUser.setEmail(expectedUser1.getEmail());
+        updatedUser.setPassword(expectedUser1.getPassword());
+        updatedUser.setPasswordConfirmation(expectedUser1.getPasswordConfirmation());
+        updatedUser.setRole(expectedUser1.getRole());
+
+        when(userService.fetchUser(1)).thenReturn(expectedUser1);
+        AppUser appUser = userService.fetchUser(1);
+        assertEquals(appUser, expectedUser1);
+
+        MvcResult mvcResult = mockMvc.perform(post("/panel/profile/update")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .flashAttr("user", updatedUser))
+                .andExpect(status().is(200))
+                .andExpect(model().attributeExists("user"))
+                .andExpect(model().attribute("user", updatedUser))
+                .andExpect(model().hasNoErrors())
+                .andReturn();
+
+        when(userService.fetchUser(1)).thenReturn(updatedUser);
+        appUser = userService.fetchUser(1);
+        assertEquals(appUser, updatedUser);
+
+        ModelAndView modelAndView = mvcResult.getModelAndView();
+        assertViewName(modelAndView, "profile-update-success");
+        verify(userService, times(1)).saveUser(updatedUser);
     }
 }
