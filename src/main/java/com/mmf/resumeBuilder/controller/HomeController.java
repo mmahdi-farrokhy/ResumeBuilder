fe1be8d4955c227f2390c1b5c5851941023c4c99
mmahdi-farrokhy <mmahdifarrokhy@gmail.com>
2024-03-05 23:25:46 +0330

    - UPDATED

--- src/main/java/com/mmf/resumeBuilder/controller/HomeController.java
+++ src/main/java/com/mmf/resumeBuilder/controller/HomeController.java
@@ -1,12 +1,14 @@
 package com.mmf.resumeBuilder.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
 
-@Controller
+@RestController
+@RequestMapping("/api/v1")
 public class HomeController {
 
     @GetMapping("/home")
