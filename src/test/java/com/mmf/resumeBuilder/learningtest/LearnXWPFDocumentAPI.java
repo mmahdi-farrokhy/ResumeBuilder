9f6a15e9d4cb7d6745b96c75c1c8945cf99ff796
mmahdi-farrokhy <mmahdifarrokhy@gmail.com>
2024-03-23 18:51:20 +0330

    - created LearnXWPFDocumentAPI test class to learn working with the XWPFDocument API
    - added word templates o the project but not yet customized them

---
+++ src/test/java/com/mmf/resumeBuilder/learningtest/LearnXWPFDocumentAPI.java
@@ -0,0 +1,28 @@
+package com.mmf.resumeBuilder.learningtest;
+
+import com.mmf.resumeBuilder.DatabaseTest;
+import com.mmf.resumeBuilder.entity.resume.Resume;
+import com.mmf.resumeBuilder.service.DocumentGenerator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class LearnXWPFDocumentAPI {
+
+    Resume resume;
+
+    @BeforeEach
+    void setUp() {
+        resume = DatabaseTest.createResume();
+        resume.setId(1);
+    }
+
+    @Test
+    void learn_word_generation_api() {
+        DocumentGenerator.generateDocument(resume);
+    }
+
+    @Test
+    void create_a_new_word_document_and_write_inside_it() {
+        DocumentGenerator.createNewWordDocument(resume);
+    }
+}
