60ab311bf1e9dd13e522a086b02cee14c431a1e9
mmahdi-farrokhy <mmahdifarrokhy@gmail.com>
2024-03-25 23:09:36 +0330

    - created ResumeTheme enum to select word file theme

---
+++ src/main/java/com/mmf/resumeBuilder/constants/ResumeTheme.java
@@ -0,0 +1,10 @@
+package com.mmf.resumeBuilder.constants;
+
+public enum ResumeTheme {
+    ATSClassic,
+    Bold_Modern,
+    Classic_Accounting,
+    Creative_Teaching,
+    Simple_Florist,
+    Woodworking
+}
