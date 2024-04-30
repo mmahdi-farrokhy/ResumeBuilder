00297ba0cab7aaad994c682e30ef69bf1346fec6
mmahdi-farrokhy <mmahdifarrokhy@gmail.com>
2024-04-30 23:00:35 +0330

    - removed themes Bold Modern and Creative Teaching from ResumeTheme

--- src/main/java/com/mmf/resumeBuilder/constants/ResumeTheme.java
+++ src/main/java/com/mmf/resumeBuilder/constants/ResumeTheme.java
@@ -2,9 +2,7 @@ package com.mmf.resumeBuilder.constants;
 
 public enum ResumeTheme {
     ATSClassic,
-    Bold_Modern,
     Classic_Accounting,
-    Creative_Teaching,
     Simple_Florist,
     Woodworking
 }
